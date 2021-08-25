f517a8e59b14c5c67789fef80c0c8418dc95868d
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-08-25 14:44:59 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/cpp/libs/EThread
-# Build directory: C:/Dev/cpp/libs/EThread/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
