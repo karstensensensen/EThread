01c89479d162ed19536a3984ae52a9e85552fc57
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2021-08-30 21:17:35 +0200

    added deconstructor to EThread

--- include/EThread.h
+++ include/EThread.h
@@ -5,16 +5,26 @@
 
 namespace ETH
 {
+
+	typedef std::thread::id ETHID;
+
 	// A simple wrapper for a std::tread instance
-	// allows control over when the thread starts and stops outside of class initialization
+	// FEATURES:
+	// 
+	//		allows control over when the thread starts and stops outside of class initialization using start(), restart() and stop functions
+	//
+	//		auto joins on deconstruction unless ETH_NO_AUTO_JOIN is defined
+	//
 	// also acts as a base class for all other extended thread types
+	//
 	class EThread
 	{
 	public:
 
 		EThread() = default;
-		EThread(EThread&& other);
+		EThread(EThread&& other) noexcept;
 		EThread(const EThread& other);
+		~EThread();
 
 		template<typename TFunc, typename ... Args>
 		EThread(std::function<TFunc> thread_function, Args ... function_args);
@@ -24,7 +34,7 @@ namespace ETH
 
 		// for class methods
 		template<typename TFunc, typename TClass, typename ... Args, std::enable_if_t<std::is_member_function_pointer_v<TFunc> && std::is_class_v<TClass>, bool> = false>
-		EThread(TFunc thread_method, TClass* class_ptr, Args ... function_args) : EThread(std::function(std::bind(thread_method, class_ptr, function_args...))) {}
+		EThread(TFunc thread_method, TClass* class_ptr, Args ... function_args) : EThread(std::function<void()>(std::bind(thread_method, class_ptr, function_args...))) {}
 
 		template<typename TFunc, typename ... Args>
 		void setThrdFunc(std::function<TFunc> thread_fucntion, Args ... function_args);
@@ -43,13 +53,19 @@ namespace ETH
 		bool running() { return m_is_running; };
 		bool joinable() { return m_thread.joinable(); }
 
+		void detach();
+		bool detached() { return m_is_detached; }
+
 		void swap(EThread& other);
 
+		ETHID getId() { return m_thread.get_id(); }
+
 	protected:
-		
+
 		std::thread m_thread;
 		std::function<void()> m_function;
 		bool m_is_running = false;
+		bool m_is_detached = false;
 	};
 }
 
--- src/EThread.cpp
+++ src/EThread.cpp
@@ -2,13 +2,21 @@
 
 namespace ETH
 {
-	EThread::EThread(EThread&& other)
+	EThread::EThread(EThread&& other) noexcept
 		: m_thread(std::move(other.m_thread)), m_function(std::move(other.m_function)), m_is_running(other.m_is_running)
 	{}
 
 	EThread::EThread(const EThread& other)
 		: m_function(other.m_function)
 	{}
+
+	EThread::~EThread()
+	{
+	#ifndef ETH_NO_AUTO_JOIN
+		if (running() && !detached())
+			join();
+	#endif
+	}
 
 	void EThread::start()
 	{
@@ -38,7 +46,18 @@ namespace ETH
 		assert(joinable() && running());
 	#endif
 
+		m_is_running = false;
 		m_thread.join();
+	}
+
+	void EThread::detach()
+	{
+	#ifdef ETH_DEBUG
+		assert(running());
+	#endif
+
+		m_is_detached = true;
+		m_thread.detach();
 	}
 
 	void EThread::swap(EThread& other)
